a1ccea52d5afdd36b871e5038e96027835b29e20
Aleksnader.G <425730@student.fontys.nl>
2021-04-17 16:14:20 +0200

    polished the algotithms

--- src/kernelization.py
+++ src/kernelization.py
@@ -1,106 +1,85 @@
 import copy
 import random
 
-def independant_set(adj_matrix):
-
-    local_matrix =  copy.deepcopy(adj_matrix)   
+def isolated_vertices(adj_matrix):
 
     result = []
 
     for v in range(len(adj_matrix)):
-        if 1 in local_matrix[v]:
-            continue
-        else:
+        if 1 not in adj_matrix[v]:
             result.append(v)
 
     return result
 
-def pendant_vetices(adj_matrix):
-
-    local_matrix =  copy.deepcopy(adj_matrix)  
+def pendant_vertices(adj_matrix):
 
     result = []
 
     for v in range(len(adj_matrix)):
 
-        if 1 in adj_matrix[v]:
-            local_matrix[v].remove(1)
-
-            if 1 in local_matrix[v]:
-                continue
-            else:
-                result.append(v)
-
-            
+        if adj_matrix[v].count(1) == 1:
+            result.append(v)
 
     return result
 
-def tops_vetices(adj_matrix,num_edges_needed):
-
-    local_matrix =  copy.deepcopy(adj_matrix)  
+def tops_vertices(adj_matrix,num_edges_needed):
 
     result = []
 
     for v in range(len(adj_matrix)):
-        num_edges_in_vertex = 0
 
-        for i in range(len(adj_matrix)):
-            if adj_matrix[v][i] == 1:
-                num_edges_in_vertex +=1
+        num_edges_in_vertex = adj_matrix[v].count(1)
 
         if num_edges_in_vertex > num_edges_needed:
             result.append(v)
 
     return result
 
+def make_pendant_vertex(adj_matrix):
 
-def make_pendant_vertex(adj_matrix,vetrex):
+    all_tops_vertices = tops_vertices(adj_matrix,1)
+    vertex = all_tops_vertices[random.randint(0,len(all_tops_vertices)-1)]
 
     local_matrix =  copy.deepcopy(adj_matrix)  
 
-    num_edges = 0
-    for i in range(len(local_matrix[vetrex])):
-        if local_matrix[vetrex][i] == 1:
-            num_edges +=1
-        else:
-            pass
+    num_edges = adj_matrix[vertex].count(1)
     
-    for i in range(len(local_matrix[vetrex])):
-        if local_matrix[vetrex][i] == 1:
-            local_matrix[vetrex][i] = 0
+    for i in range(len(local_matrix[vertex])):
+        if local_matrix[vertex][i] == 1:
+            local_matrix[vertex][i] = 0
             num_edges -=1
         if num_edges == 1:
             break
 
     return local_matrix
     
-            
-def make_tops_vetrex(adj_matrix,vertex):
+def make_tops_vetrex(adj_matrix,k):
+
+    random.seed()
+    all_tops_vertices = tops_vertices(adj_matrix,k)
+    vertex = random.randint(0,len(adj_matrix)-1)
+
+    while vertex in all_tops_vertices:
+        vertex = random.randint(0,len(adj_matrix)-1)
 
     local_matrix =  copy.deepcopy(adj_matrix) 
-
-    num_edges = random.randrange(1,len(adj_matrix)-1)
+    num_edges = k - local_matrix[vertex].count(1) + 1
 
     while num_edges > 0:
-
         new_edge = random.randint(0,len(adj_matrix)-1)
 
-        if adj_matrix[vertex][new_edge] == 0:
-            local_matrix[vertex][new_edge] = 1;
+        if local_matrix[vertex][new_edge] == 0 and vertex != new_edge:
+            local_matrix[vertex][new_edge] = 1
             num_edges -=1
-        else:
-            pass
 
     return local_matrix
 
 
+# graph = [[0, 1, 0, 0, 1],
+#         [1, 0, 0, 0, 0], 
+#         [0, 1, 0, 1, 0], 
+#         [0, 1, 0, 0, 0], 
+#         [0, 0, 0, 0, 0]]
 
-
-graph = [[0, 1, 0, 0, 1],
-        [1, 0, 0, 0, 0], 
-        [0, 1, 0, 1, 0], 
-        [0, 1, 1, 1, 1], 
-        [1, 0, 0, 1, 1]]
-
-print(tops_vetices(graph,2))
+# print(make_tops_vetrex(graph,3))
 
