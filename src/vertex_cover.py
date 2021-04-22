4860e066c3bb7c97167dd3855776f048b8498191
Aleksnader.G <425730@student.fontys.nl>
2021-04-22 19:41:43 +0200

    fixed the smart vertex cover bug

--- src/vertex_cover.py
+++ src/vertex_cover.py
@@ -11,6 +11,14 @@ def __generate_all_covers__(matrix_size, cover_size):
 
     return combos
 
+def __generate_all_covers2__(items, cover_size):
+    combos = []
+
+    for assignment in list(combinations(items, cover_size)):
+        combos.append(assignment)
+
+    return combos
+
 
 def __is_cover_valid__(adj_matrix_arg):
     for i in range(len(adj_matrix_arg)):
@@ -21,10 +29,16 @@ def __is_cover_valid__(adj_matrix_arg):
     return True
 
 def cover_check(adj_matrix, cover_size):
-    if len(adj_matrix[0]) == 0:
+    if len(adj_matrix) == 0:
         return set()
 
-    combinations = __generate_all_covers__(len(adj_matrix), cover_size)
+    combo_items = []
+    for i in adj_matrix:
+        if 1 in i:
+            combo_items.append(adj_matrix.index(i))
+
+    
+    combinations = __generate_all_covers2__(combo_items, cover_size)
     index_comb = len(combinations)
     for combo in combinations:
         
@@ -60,32 +74,35 @@ def smart_cover(adj_matrix, k):
     pendant_neighbours = []
     for i in pendant:
         if i not in pendant_neighbours:
-            pendant_neighbours.append(adj_matrix[i].index(1))            
+            if adj_matrix[i].index(1) not in pendant_neighbours:
+                pendant_neighbours.append(adj_matrix[i].index(1))            
 
     cover.update(pendant_neighbours)
     not_in_cover.difference_update(cover)
 
-    to_be_removed = set()
-    to_be_removed.update(cover)
-    to_be_removed.update(not_in_cover)
+    to_be_removed = []
+    to_be_removed.extend(cover)
+    to_be_removed.extend(not_in_cover)
 
-    to_be_removed2 = list(to_be_removed)
     local_matrix = copy.deepcopy(adj_matrix)
 
-    local_matrix = [i for i in local_matrix if i not in to_be_removed2]
-    local_matrix = [listEle for listEle in local_matrix if listEle != []]
-    if len(local_matrix) > 0:
-        for q in range(len(local_matrix)):
-            local_matrix[q] = [p for p in local_matrix[q] if p not in to_be_removed2]
-            local_matrix[q] = [listEle for listEle in local_matrix[q] if listEle != []]
+    for i in to_be_removed:
+            local_matrix[i] = [0]*len(adj_matrix)  
+            for j in range(len(adj_matrix)):
+                local_matrix[j][i] = 0       
 
-    
+    if k-len(cover) <= 0:
+        return set()
     cover.update(cover_check(local_matrix, k - len(cover)))
 
     if len(cover) == k:
         return cover
     else:
         return set()
+
+
+graph = [[0, 0, 1, 0, 0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 0, 1, 1, 0, 0, 0], [1, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 1, 1, 0, 0, 1, 1], [0, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 1, 0, 1, 0, 0, 0, 1, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [1, 0, 0, 1, 0, 0, 0, 0, 0, 0], [1, 0, 0, 1, 0, 0, 1, 0, 0, 0]]
+print(smart_cover(graph,4))
 
 
 
